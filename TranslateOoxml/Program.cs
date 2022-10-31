d4a02d555aaca370be52b913821660329a29b651
Igor Ignatyuk <igor_ignatiouk@hotmail.com>
2022-10-31 13:48:56 +0100

    Add language code information to the help

--- TranslateOoxml/Program.cs
+++ TranslateOoxml/Program.cs
@@ -107,8 +107,9 @@ if (args.Length == 2)
     }
 else
     Console.Error.Write(
-        $"Syntax: {GetFileNameWithoutExtension(ProcessPath)} sourceFile targetLanguage\n\n" +
+        $"Syntax: {GetFileNameWithoutExtension(ProcessPath)} sourceFile targetLanguageCode\n\n" +
         "The source file can be a .docx, .pptx, or .xlsx one.\n\n" +
         "The target file will appear in the same folder where the source file resides.\n" +
         "The target file name will have the target language as a suffix.\n\n" +
+        "Language codes: https://www.deepl.com/docs-api/translate-text/translate-text/\n\n" +
         $"The environment variable {DeepLAuthKey} should be set.\n");
