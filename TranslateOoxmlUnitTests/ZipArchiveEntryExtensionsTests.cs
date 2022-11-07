15a67c90fe965dbac7d190f2f81090477a6d8186
Igor Ignatyuk <igor_ignatiouk@hotmail.com>
2022-11-07 13:26:01 +0100

    Add test method Test_Write_Translate_Read

--- TranslateOoxmlUnitTests/ZipArchiveEntryExtensionsTests.cs
+++ TranslateOoxmlUnitTests/ZipArchiveEntryExtensionsTests.cs
@@ -16,5 +16,17 @@ namespace TranslateOoxmlUnitTests
             var s = entry.Read();
             Assert.AreEqual(s, "Test string");
         }
+
+        [TestMethod]
+        public void Test_Write_Translate_Read()
+        {
+            using var stream = new MemoryStream();
+            using var archive = new ZipArchive(stream, ZipArchiveMode.Update);
+            var entry = archive.CreateEntry("TestEntry");
+            entry.Write("Test string");
+            entry.Translate((text) => Task.FromResult(text.ToUpper())).Wait();
+            var s = entry.Read();
+            Assert.AreEqual(s, "TEST STRING");
+        }
     }
 }
