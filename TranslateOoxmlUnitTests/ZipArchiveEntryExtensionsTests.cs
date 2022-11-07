5acd601cd53268ce90206e3bc5b2ff4e896a66d4
Igor Ignatyuk <igor_ignatiouk@hotmail.com>
2022-11-07 12:43:02 +0100

    Add project TranslateOoxmlUnitTests

---
+++ TranslateOoxmlUnitTests/ZipArchiveEntryExtensionsTests.cs
@@ -0,0 +1,20 @@
+using System.IO.Compression;
+using static TranslateOoxml.ZipArchiveEntryExtensions.Extensions;
+
+namespace TranslateOoxmlUnitTests
+{
+    [TestClass]
+    public class ZipArchiveEntryExtensionsTests
+    {
+        [TestMethod]
+        public void Test_Write_Read()
+        {
+            using var stream = new MemoryStream();
+            using var archive = new ZipArchive(stream, ZipArchiveMode.Update);
+            var entry = archive.CreateEntry("TestEntry");
+            entry.Write("Test string");
+            var s = entry.Read();
+            Assert.AreEqual(s, "Test string");
+        }
+    }
+}
