e5f482ccc5bd27a66897ffba1a2a030b6e60e0ec
Igor Ignatyuk <igor_ignatiouk@hotmail.com>
2022-11-28 17:18:44 +0100

    Don't use the filename extension

--- TranslateOoxml/OoxmlTranslator.cs
+++ TranslateOoxml/OoxmlTranslator.cs
@@ -1,6 +1,5 @@
 ﻿using System.IO.Compression;
 using static System.IO.File;
-using static System.IO.Path;
 using static TranslateOoxml.ZipArchiveEntryExtensions.Extensions;
 
 namespace TranslateOoxml;
@@ -33,29 +32,33 @@ public static class OoxmlTranslator
 
         Copy(sourcePath, targetPath, true);
         using var zipArchive = ZipFile.Open(targetPath, ZipArchiveMode.Update);
-        switch (GetExtension(targetPath).ToLower())
         {
-            case ".docx":
-                {
-                    var entry = zipArchive.GetEntry("word/document.xml");
-                    if (entry != null)
-                        await entry.Translate(translate);
-                }
-                break;
-            case ".pptx":
-                foreach (var entry in zipArchive.Entries)
-                    if (entry.FullName.StartsWith("ppt/slides/slide"))
-                        await entry.Translate(translate);
-                break;
-            case ".xlsx":
-                {
-                    var entry = zipArchive.GetEntry("xl/sharedStrings.xml");
-                    if (entry != null)
-                        await entry.Translate(translate);
-                }
-                break;
-            default:
-                throw new Exception("Unsupported file format");
+            var entry = zipArchive.GetEntry("word/document.xml");
+            if (entry != null)
+            {
+                await entry.Translate(translate);
+                return;
+            }
         }
+        {
+            var slideFound = false;
+            foreach (var entry in zipArchive.Entries)
+                if (entry.FullName.StartsWith("ppt/slides/slide"))
+                {
+                    slideFound = true;
+                    await entry.Translate(translate);
+                }
+            if (slideFound)
+                return;
+        }
+        {
+            var entry = zipArchive.GetEntry("xl/sharedStrings.xml");
+            if (entry != null)
+            {
+                await entry.Translate(translate);
+                return;
+            }
+        }
+        throw new Exception("Unsupported file format");
     }
 }
