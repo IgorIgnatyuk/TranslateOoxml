e198e536afb9577184351430571e5bf119f61e89
Igor Ignatyuk <igor_ignatiouk@hotmail.com>
2023-05-27 21:10:36 +0200

    Use file-scoped namespaces consistently

--- TranslateOoxmlTestHelpers/TestHelpers.cs
+++ TranslateOoxmlTestHelpers/TestHelpers.cs
@@ -1,21 +1,20 @@
-﻿namespace TranslateOoxml.Test.Helpers
-{
-    public static class Equality
-    {
-        public static bool StreamsAreEqual(Stream stream1, Stream stream2)
-        {
-            int b;
-            while ((b = stream1.ReadByte()) == stream2.ReadByte())
-                if (b == -1)
-                    return true;
-            return false;
-        }
+﻿namespace TranslateOoxml.Test.Helpers;
 
-        public static bool FilesAreEqual(string path1, string path2)
-        {
-            using var stream1 = File.OpenRead(path1);
-            using var stream2 = File.OpenRead(path2);
-            return StreamsAreEqual(stream1, stream2);
-        }
+public static class Equality
+{
+    public static bool StreamsAreEqual(Stream stream1, Stream stream2)
+    {
+        int b;
+        while ((b = stream1.ReadByte()) == stream2.ReadByte())
+            if (b == -1)
+                return true;
+        return false;
+    }
+
+    public static bool FilesAreEqual(string path1, string path2)
+    {
+        using var stream1 = File.OpenRead(path1);
+        using var stream2 = File.OpenRead(path2);
+        return StreamsAreEqual(stream1, stream2);
     }
 }
